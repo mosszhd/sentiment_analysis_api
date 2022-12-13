5d9243759c6faf277b89dcce490d55913359d6ad
mosszhd <mustafakamalmoss@gmail.com>
2022-12-13 20:49:36 +0600

    my first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from src.main import Prediction
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'This is my first API call!'
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    prediction_obj = Prediction()
+    input_json = request.get_json(force=True) 
+    print('nput json',input_json)
+    result = prediction_obj.get_prediction(input_json['text'])
+    dictToReturn = {'Prediction':result}
+    return jsonify(dictToReturn)
+
+app.run()
---
+++ src/main.py
@@ -0,0 +1,22 @@
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+class Prediction:
+    def __init__(self):
+        self.model_path = "src/model/BNBmodel.sav"
+        self.vectorizer_filename = 'src/model/count_dict.sav'
+        self.loaded_model = pickle.load(open(self.model_path, 'rb'))
+        self.vectoriser = pickle.load(open(self.vectorizer_filename, 'rb'))
+
+    
+    def get_prediction(self,user_input):
+        user_input = user_input
+        vectorized_input = self.vectoriser.transform([user_input])
+        result = self.loaded_model.predict(vectorized_input)
+        print(result)
+        if result[0] == 1:
+            return "positive"
+        else:
+            return "negative"
+
